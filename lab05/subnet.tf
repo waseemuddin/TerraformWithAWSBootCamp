a4241b071595be74803cb290e55134d919210845
ShaikhWaseem <waseem.uddin@live.com>
2025-02-07 21:37:19 +0500

    Delete lab05 directory
    lab05 deleted

--- lab05/subnet.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_subnet" "mycustom-subnet" {
-#     vpc_id = aws_vpc.mycustom-vpc.id
-#     //cidr_block = "10.0.10.0/24"1
-#     cidr_block = var.mysubnet
-#     tags = {
-#         Name: "${var.env_prefix} - Custom-subnet-1"
-#     }
-# }
-
--- lab05/terraform.tfvars
+++
@@ -1,9 +0,0 @@
-# myvpc = "10.0.0.0/16"
-# mysubnet = "10.0.10.0/24"
-# ami_id = "ami-04b4f1a9cf54c11d0"
-# instance_type = "t2.micro"
-# env_prefix = "CVPC"
-# avail_zone = "us-east-1c"
-# my_ip =  "<my_ip>"
-# myinstance_type = "t2.micro"
-
