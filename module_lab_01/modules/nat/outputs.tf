6582c0295700ea213eb8d6c1ff13ed34a58a6df2
ShaikhWaseem <waseem.uddin@live.com>
2025-02-07 21:04:05 +0500

    Delete module_lab_01 directory

--- module_lab_01/modules/nat/outputs.tf
+++
@@ -1,3 +0,0 @@
-output natgw_id {
-  value       = aws_nat_gateway.natgw.id
-}
--- module_lab_01/modules/network/variables.tf
+++
@@ -1,9 +0,0 @@
-//variable aws_region {}
-variable vpc_name {}
-variable vpc_cidr {}
-variable environment {}
-variable public_cidr_block {}
-variable private_cidr_block {}
-variable avail_zone {}
-variable natgw_id {}
-
