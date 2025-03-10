dfa4828fe055a6974694b5cf533429517bc001b2
Waseemuddin Shaikh <waseem.uddin@live.com>
2025-03-10 22:53:12 +0500

    terraform varialbale class

--- 1st_lab_tf/main.tf
+++ 1st_lab_tf/main.tf
@@ -1,115 +1,78 @@
 
-provider "aws" {
-  region = "us-east-1"
-  //access_key = ""
-  //secret_key = ""
-}
-//Variables Defining Class
-//1st way to define variable
-# variable "subnet_cidr_block" {
-#   description = "subnet cidr block"
-#   //default = "10.0.10.0/24"
-#   //type = list(string)
+
+# resource "aws_instance" "myprod-machine" {
+#     ami = "ami-04b4f1a9cf54c11d0"
+#     instance_type = "t2.micro"
+#     tags = {
+#       Name: "Production-machine"
+#     }
 # }
-
-variable "cidr_blocks" {
-  description = "subnet cidr block"
-  //default = "10.0.10.0/24"
-  //type = list(string)
-  type = list(object({
-  cidr_block = string
-  name = string
-}))
-
-}
 
 # variable "vpc_cidr_block" {
 #   description = "vpc cidr block"
 # }
-//2nd way to defining variables
-//   terraform plan -var "subnet_cidr_block=10.0.1.0/24"///
+variable "cidr_blocks" {
+  description = "cidr blocks for vpc and subnet"
+  //default = "10.0.10.0/24"
+  type = list(object({
+    cidr_block = string
+    name = string
+  }))
+  
+}
+ 
 
-# variable "environment" {
-#   description = "deployment environment"
-
+# variable "subnet_cidr_block" {
+#   description = "subnet cidr block"
+#   //default = "10.0.10.0/24"
+#   type = list(sting)
 # }
+ 
+variable "environment" {
+  description = "production"
+}
 
-resource "aws_vpc" "dev-vpc" {
+resource "aws_vpc" "mycustom-vpc" {
+  //cidr_block = "10.0.0.0/16"
+    cidr_block = var.cidr_blocks[0].cidr_block
   //cidr_block = var.vpc_cidr_block
-  cidr_block = var.cidr_blocks[0].cidr_block
+  //environment = var.environment
   tags = {
- //   Name : var.environment
- Name: var.cidr_blocks[0].name
+    Name: var.cidr_blocks[0].name
+    }
+}
+
+resource "aws_subnet" "mycustom-subnet-1" {
+  vpc_id = aws_vpc.mycustom-vpc.id
+  //cidr_block = "192.168.1.0/24"
+  //cidr_block = var.subnet_cidr_block  
+  cidr_block = var.cidr_blocks[1].cidr_block
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name: var.cidr_blocks[1].name
   }
 }
 
-resource "aws_subnet" "dev-subnet-01" {
-  vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = var.cidr_blocks[1].cidr_block
-  availability_zone = "us-east-1a"
-}
-
-
-# resource "aws_subnet" "dev-subnet-01" {
-#   vpc_id            = aws_vpc.dev-vpc.id
-#   cidr_block        = var.cidr_blocks[1]
-#   availability_zone = "us-east-1a"
-# }
-
-# resource "aws_subnet" "dev-subnet-01" {
-#   vpc_id            = aws_vpc.dev-vpc.id
-#   cidr_block        = var.subnet_cidr_block
-#   availability_zone = "us-east-1a"
-# }
-
-
-
-# //Lab - 01 Terraform Basic
-# ##
-# provider "aws" {
-#   region = "us-east-1"
-#   //access_key = ""
-#   //secret_key = ""
-# }
-
-# resource "aws_vpc" "my-cvpc" {
-
-#   cidr_block = "192.168.0.0/16"
-#   tags = {
-#     Name : "C-VPC Block"
-#   }
-# }
-
-# resource "aws_subnet" "aws_subnet-1" {
-
-#   vpc_id            = aws_vpc.my-cvpc.id
-#   cidr_block        = "192.168.1.0/25"
-#   availability_zone = "us-east-1a"
-#   tags = {
-#     Name : "C-Subnet-1"
-#   }
-# }
-
-# data "aws_vpc" "existing_vpc" {
+# data "aws_vpc" "existing_vpc2" {
 #   default = true
 # }
 
-# resource "aws_subnet" "ex-subnet-2" {
-#   vpc_id            = data.aws_vpc.existing_vpc.id
-#   cidr_block        = "172.31.96.0/20"
-#   availability_zone = "us-east-1c"
-#   tags = {
-#     Name : "C-Subnet-2"
-#   }
+# resource "aws_subnet" "mycustom-subnet-2" {
+#     vpc_id = data.aws_vpc.existing_vpc2.id
+#     cidr_block = "172 .31.96.0/24"
+#     availability_zone = "us-east-1c"
 # }
 
-# //Printed out the output value while terraform apply command.
+# resource "aws_s3_bucket" "example" {
+#   bucket = "my-terraform-bucket"
+#   acl    = "private"
+
 # output "dev-vpc_id" {
-#   value = aws_vpc.my-cvpc.id
+#   value = aws_vpc.mycustom-vpc.id
 # }
 
 
-# output "dev-subnet_id" {
-#   value = aws_subnet.aws_subnet-1.id
-# }
-
+# output "dev-subnet-id" {
+#   value = aws_subnet.mycustom-subnet-1.id
+#    }
--- 1st_lab_tf/terraform.tfvars
+++ 1st_lab_tf/terraform.tfvars
@@ -4,7 +4,13 @@
 //vpc_cidr_block    = "10.0.0.0/16"
 //environment        = "development"
 //cidr_blocks = ["10.0.10.0/24","10.0.20.0/24"]
+
 cidr_blocks = [
     {cidr_block = "10.0.0.0/16", name = "dev-vpc"},
     {cidr_block = "10.0.10.0/24", name = "dev-subnet"}
 ]
+
+//vpc_cidr_block = "192.168.0.0/16"
+
+//subnet_cidr_block = "192.168.1.0/24"
+environment = "development"
